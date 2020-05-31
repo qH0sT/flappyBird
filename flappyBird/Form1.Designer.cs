6129fffadd88daecd7feda4e12165fc7470de998
qH0sT <asuslaptop230@gmail.com>
2020-05-31 22:55:05 +0300

    Filapi Börd

---
+++ flappyBird/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace flappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(164, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 403);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Puan: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(27, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 456);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Filapi Börd C#";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ flappyBird/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flappyBird
+{
+    public partial class Form1 : Form     // GÖZLERİM SANSÜR PERDESİİİİİ. https://www.youtube.com/watch?v=JhquGf1pi_Y
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.ImageLocation =Environment.CurrentDirectory + "\\Res\\bird.gif";
+        }
+        bool yukari = false;
+        List<PictureBox> pictirBaks = new List<PictureBox>();
+        private async void assagiIndir()
+        {
+            await Task.Run(async () => {
+                while (yukari == false)
+                {
+                    Invoke((MethodInvoker)delegate {
+                        pictureBox1.Top += 5;
+                    });
+                    await Task.Delay(1);
+                }
+            });
+        }
+        private async void KolonHareket()
+        {
+            await Task.Run(async () => {
+                while (true)
+                {
+                    Invoke((MethodInvoker)delegate {
+                        foreach (PictureBox pcbx in pictirBaks)
+                        {
+                            if (pictirBaks.Count % 2 == 0)
+                            {
+                                pcbx.Left -= 40;
+                                pcbx.Left -= 40;
+                            }
+                        }
+                        foreach (PictureBox pcbx in point)
+                        {
+                         
+                                pcbx.Left -= 40;
+                                pcbx.Left -= 40;
+                           
+                        }
+                    });
+
+                    if (pictirBaks.Any(x => pictureBox1.Bounds.IntersectsWith(x.Bounds)))
+                    {
+                        pictureBox1.BackColor = Color.Green;
+                        label2.Text = "Yandınız.";
+                        break;
+                    }
+                     if (point.Any(d => pictureBox1.Bounds.IntersectsWith(d.Bounds)))
+                    {
+                        puan += 10;
+                        if (puan % 3 == 0)
+                        {
+                            Invoke((MethodInvoker)delegate {
+                                foreach (var b in pictirBaks)
+                                {
+                                    Controls.Remove(b);
+                                }
+                                foreach (var b in point)
+                                {
+                                    Controls.Remove(b);
+                                }
+                                point.Clear();
+                                pictirBaks.Clear();
+                              
+                            label2.Text = "Seviye atlandı";
+                            kolonlariDiz();
+                            });
+                        }
+                      
+                        label1.Text = puan.ToString();
+                    }
+                   
+                    await Task.Delay(200);
+                    label2.Text = "...";
+                    
+                }
+            });
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                yukari = true;
+                pictureBox1.Top -= 65;
+            }
+        }
+        private void kolonlariDiz()
+        {
+            PictureBox pcbX = null;
+            PictureBox puantaj = null;
+            for (int i = 0; i < 6; i++)
+            {
+                pcbX = new PictureBox();
+                pcbX.BackColor = Color.Green;
+
+                if (i % 2 == 0)
+                {
+                    pcbX.Top = 0;
+                    pcbX.Left = Size.Width;
+                    switch (tutucu)
+                    {
+                        case 0:
+                            pcbX.Size = new Size(68, 153 + 400);
+                            break;
+                        case 1:
+                            pcbX.Top = -100;
+                            pcbX.Size = new Size(68, 153);
+                            break;
+                        case 2:
+                            pcbX.Size = new Size(68, 153 + 100);
+                            break;
+                    }
+
+                }
+                else
+                {
+                    pcbX.Left = Size.Width;
+                    puantaj = new PictureBox();
+                    puantaj.Top = 0;
+                    puantaj.BackColor = Color.Red;
+                    puantaj.Size = new Size(40, 700);
+                    puantaj.Visible = false;
+                    puantaj.Left = pcbX.Left + 100;
+                    point.Add(puantaj);
+                    Controls.Add(puantaj);
+                    switch (tutucu)
+                    {
+                        case 0:
+                            pcbX.Top = Size.Height - 100;
+                            pcbX.Size = new Size(68, 153 + 500);
+                            break;
+                        case 1:
+                            pcbX.Top = Size.Height - 600;
+                            pcbX.Size = new Size(68, 153 + 500);
+                            break;
+                        case 2:
+                            pcbX.Top = Size.Height - 400;
+                            pcbX.Size = new Size(68, 153 + 400);
+                            break;
+                    }
+                    if ((i + 1) % 2 == 0) { tutucu = rnd.Next(0, 2); }
+
+                }
+                Controls.Add(pcbX);
+                pictirBaks.Add(pcbX);
+
+                foreach (Control cntrl in Controls)
+                {
+                    if (cntrl is PictureBox)
+                    {
+                        if (cntrl.Name != "pictureBox1")
+                        {
+                            if (pictirBaks.Count % 2 == 0)
+                            {
+                                cntrl.Left += 780;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int tutucu = 1;
+        int puan = 0;
+        Random rnd = new Random();
+        List<PictureBox> point = new List<PictureBox>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kolonlariDiz();
+            assagiIndir();
+            KolonHareket();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                yukari = false;
+                assagiIndir();
+            }
+        }
+    }
+}
